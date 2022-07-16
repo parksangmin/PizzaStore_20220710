cdf210fe252231a6b2656c0263f8670661fa00a0
박 상 민 <dhwkffks7@naver.com>
2022-07-16 11:07:52 +0900

    한 칸에 해당하는 가게를 나타내기 위한 DetailStoreActivity.kt 생성

---
+++ app/src/main/java/com/sangmin/pizzastore_20220710/DetailStoreActivity.kt
@@ -0,0 +1,11 @@
+package com.sangmin.pizzastore_20220710
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_store)
+    }
+}
--- app/src/main/java/com/sangmin/pizzastore_20220710/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/sangmin/pizzastore_20220710/Fragments/ProfileFragment.kt
@@ -11,7 +11,7 @@ import com.sangmin.pizzastore_20220710.ProfileActivity
 import com.sangmin.pizzastore_20220710.R
 import kotlinx.android.synthetic.main.fragment_profile.*
 
-class ProfileFragment: Fragment() {
+class ProfileFragment : Fragment() {
 
 //    숫자값으로 코드에 적으면, 이 숫자가 무슨? 어떤 데이터를 받으러 가는지 코드를 읽고 이해하기 어렵다
 //    코드의 가독성을 올리기 위해 맴버변수로 바꾼다(변수명을 이해할 수 있게 작성)
@@ -24,7 +24,7 @@ class ProfileFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container,false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
 
 
     }
@@ -40,26 +40,46 @@ class ProfileFragment: Fragment() {
 
         changephonNumBtn.setOnClickListener {
             val myIntent = Intent(requireContext(), ProfileActivity::class.java)
-            startActivityForResult(myIntent,REQ_FOR_PHONENUM)
+            startActivityForResult(myIntent, REQ_FOR_PHONENUM)
         }
     }
 
 
-
-
-//    복귀한 A화면 -> B화면에서 입력한 데이터를 받아서  UI 반영 등 코드 작성
+    //    복귀한 A화면 -> B화면에서 입력한 데이터를 받아서  UI 반영 등 코드 작성
 //    requestCode : 어떤 데이터를 가져온건지 구별해주는 숫자(1)에서 작성한 숫자
 //    resultCode : 확인버튼을 누른게 맞는가? Activity.Result_OK
 //    data? : 결과로 세팅해준 resultintent를 담고 있는 변수
 //    data?.get자료형Extra로 넣어둔 데이터를 추출
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQ_FOR_NICKNAME){
-            if(resultCode == Activity.RESULT_OK) {
+//        if (requestCode == REQ_FOR_NICKNAME){
+//            if(resultCode == Activity.RESULT_OK) {
+//                val changedNick = data?.getStringExtra("data")
+//                nicknameTxt.text = changedNick
+//            }
+//
+//        }
+//
+//    if(requestCode == REQ_FOR_PHONENUM){
+//        if(resultCode == Activity.RESULT_OK){
+//            val changePhoneNum = data?.getStringExtra("data")
+//            phoneNumTxt.text = changePhoneNum
+//        }
+//    }
+//
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQ_FOR_NICKNAME) {
                 val changedNick = data?.getStringExtra("data")
                 nicknameTxt.text = changedNick
-            }
 
+
+            }
+            if (resultCode == REQ_FOR_PHONENUM) {
+                val changePhoneNum = data?.getStringExtra("data")
+                phoneNumTxt.text = changePhoneNum
+
+            }
         }
 
     }
